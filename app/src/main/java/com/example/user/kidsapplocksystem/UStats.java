58962aee9903164082bf3023c7dfdd9de940ec79
Eng_A <Eng_alshugaaye@hotmail.com>
2020-05-22 13:20:26 +0800

    Initial commit

---
+++ app/src/main/java/com/example/user/kidsapplocksystem/UStats.java
@@ -0,0 +1,35 @@
+package com.example.user.kidsapplocksystem;
+
+import android.annotation.TargetApi;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+import java.util.Calendar;
+import java.util.List;
+
+/**
+ * Created by zack on 06/03/2017.
+ */
+
+public class UStats {
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public static List<UsageStats> getUsageStatsList(Context context) {
+        UsageStatsManager usm = getUsageStatsManager(context);
+        Calendar calendar = Calendar.getInstance();
+        long endTime = calendar.getTimeInMillis();
+        calendar.add(Calendar.YEAR, -1);
+        long startTime = calendar.getTimeInMillis();
+
+        return usm.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, startTime, endTime);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @SuppressWarnings("ResourceType")
+    private static UsageStatsManager getUsageStatsManager(Context context) {
+        return (UsageStatsManager) context.getSystemService("usagestats");
+    }
+}
